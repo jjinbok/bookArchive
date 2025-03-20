65eabceec91d4dd66277a7bc871f48b5648c3271
jjinbok <haba8730@naver.com>
2025-03-21 02:19:08 +0900

    chore : add default file

---
+++ src/main/java/backend/bookArchive/BookArchiveApplication.java
@@ -0,0 +1,13 @@
+package backend.bookArchive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookArchiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookArchiveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/backend/bookArchive/BookArchiveApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.bookArchive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookArchiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
